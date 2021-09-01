21d8b826d5ed5f5c88b3197c338db4e8b3abb232
cjm <2422886222@qq.com>
2021-09-02 00:16:28 +0800

    This is first commit.

---
+++ demo01/src/com/action/LoginAction.java
@@ -0,0 +1,11 @@
+package com.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport{
+	
+	@Override
+	public String execute() throws Exception{
+		return SUCCESS;
+	}
+}
